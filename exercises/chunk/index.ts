6c11b52baf1454cec4b450348feb104543b9f432
hashimali13 <hashimwars@gmail.com>
2022-01-19 22:25:56 +0000

    chunk

--- exercises/chunk/index.ts
+++ exercises/chunk/index.ts
@@ -1,4 +1,3 @@
-"use strict";
 // --- Directions
 // Given an array and chunk size, divide the array into many subarrays
 // where each subarray is of length size
@@ -8,21 +7,24 @@
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
-function chunk(array, size) {
+
+function chunk(array: number[], size: number) {
     let largerArray = [];
     let tempArray = [];
-    for (let num of array) {
-        console.log(tempArray.length);
-        if (tempArray.length < size) {
-            tempArray.push(num);
-        }
-        else {
-            largerArray.push(tempArray);
+    for(let num of array){
+        
+        console.log(tempArray.length)
+        if(tempArray.length<size){
+            tempArray.push(num)
+        }else{
+            largerArray.push(tempArray)
             tempArray = [];
-            tempArray.push(num);
+            tempArray.push(num)
         }
     }
     largerArray.push(tempArray);
     return largerArray;
 }
+
+
 module.exports = chunk;
